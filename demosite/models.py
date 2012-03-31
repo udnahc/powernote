20046b6af2ecb4308d1c5bf4e9f3b4098b8efcb5
halesh <halesh@halesh-desktop.(none)>
2012-03-31 22:13:30 +0530

    Modified the demo code

--- demosite/models.py
+++ demosite/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class DemoUser(models.Model):
+    username = models.CharField(max_length=200)
+    personal_email = models.EmailField(unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now = True)
--- poweruser/views.py
+++ poweruser/views.py
@@ -3,10 +3,37 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.utils import simplejson
 from django.core import serializers
-
+from poweruser.forms import LoginForm
+from demosite.models import DemoUser
 
 def view_sign_up(request, signup_page_template):
     return render_to_response( signup_page_template, locals())
 
 def view_login_in(request, login_page_template):
-    return render_to_response( login_page_template, locals())
+
+    def errorHandle(error):
+        form = LoginForm()
+        return render_to_response(login_page_template, {
+                          'error' : error,
+                          'form' : form,
+        })
+    if request.method == 'POST': # If the form has been submitted...                                                                                         
+        form = LoginForm(request.POST) # A form bound to the POST data                                                                                       
+        if form.is_valid(): # All validation rules pass
+            username = request.POST['username']
+            email = request.POST['email']
+            demo_user = DemoUser()
+            demo_user.username = username
+            demo_user.personal_email = email
+            demo_user.save()
+            return render_to_response('demo/welcome_page.html', {
+                            'username': username,
+                            })
+        else:
+            error = u'Username and Password should not be empty'
+            return errorHandle(error)
+    else:
+        form = LoginForm() # An unbound form                                                                                                                 
+        return render_to_response(login_page_template, {
+                'form': form,
+                })
